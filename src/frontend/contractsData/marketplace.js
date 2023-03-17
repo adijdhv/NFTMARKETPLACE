087cbf6c590cec4f04af6d745eb5760855e2b36f
Aadesh Jadhav <55881613+adijdhv@users.noreply.github.com>
2023-03-17 20:57:40 +0530

    Update marketplace.js

--- src/frontend/contractsData/marketplace.js
+++ src/frontend/contractsData/marketplace.js
@@ -1,4 +1,4 @@
  
-export const marketplaceaddress = "0xB868601D387Dc0f40dbd735927A7cF9046Be545B"
+export const marketplaceaddress = "0x909c9DaE95C1b3f018a789A19b97Cb84c900b03d"
  // "EXPLORER" :"https://testnet.snowtrace.io/address/0x097a1903ea1ae6c5329e32600ecf2fcfee7a6a88#code"
  
