da362295711b9eaa012bdbafd805b3abd6afc364
Aadesh Jadhav <55881613+adijdhv@users.noreply.github.com>
2023-03-17 23:59:43 +0530

    Update NFTaddress.js

--- src/frontend/contractsData/NFTaddress.js
+++ src/frontend/contractsData/NFTaddress.js
@@ -1,6 +1,6 @@
 
  
   
-export const NFTaddress = "0x70D8FAE82b3c2F880F1cF4cD82D6BD390f2729Ac"
+export const NFTaddress = "0xA66084D01482e3CA951e5D7340FD4A1aDB3a95a3"
   //"EXPLORER": "https://testnet.snowtrace.io/address/0xc72a06ff0cfa07bde77605a359099d8b1b081f27#code"
  
